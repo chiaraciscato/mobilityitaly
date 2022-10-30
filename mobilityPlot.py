18d07b78c0544e8d0e8bea4bcc21a46667051aba
Chiara Ciscato <chiaraciscato@MacBook-Air.local>
2022-10-30 10:07:20 +0100

    first commit

---
+++ mobilityPlot.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 29 13:49:16 2022
+
+@author: chiaraciscato
+"""
+
+import os 
+os.getcwd()
+os.chdir(r'/Users/chiaraciscato/Desktop/mobilityitaly')
+
+
